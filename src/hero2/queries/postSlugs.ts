afeda1aad25b4a68dac575ed94b3a7fb7f33ee72
Chachris <tanza145@gmail.com>
2025-09-22 10:15:01 +0700

    FinishV1

--- src/hero2/queries/postSlugs.ts
+++
@@ -1,8 +0,0 @@
-const POST_SLUGS = /* GraphQL */ `
-  query POST_SLUGS($lang: LanguageCodeFilterEnum!, $first: Int = 100) {
-    posts(where: { language: $lang }, first: $first) {
-      nodes { slug }
-    }
-  }
-`;
-export default POST_SLUGS;
